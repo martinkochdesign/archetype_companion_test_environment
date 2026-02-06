772a9f55b5b9296fb4788fc7eb133ce47d0d8524
martinkochdesign <72647122+martinkochdesign@users.noreply.github.com>
2026-02-06 11:09:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //INITIATE CONSTANTS and GLOBAL VARIABLES *****************************************************************************************
-const version = '0.71.0-beta';
+const version = '0.72.0-beta';
 
 let newNodes = []
 
